5b8aca35f7f731f6eedad9774c268de24c012bc7
magalycajas <49215824+magalycajas@users.noreply.github.com>
2020-12-08 23:33:02 -0500

    Update Deposito_Retiro.java
    commit

--- Proyecto Interciclo/banco/src/main/java/ec/edu/ups/appdis/g1/modelo/Deposito_Retiro.java
+++ Proyecto Interciclo/banco/src/main/java/ec/edu/ups/appdis/g1/modelo/Deposito_Retiro.java
@@ -15,4 +15,42 @@ public class Deposito_Retiro {
 	private Cuenta cuenta;
 	private Cliente cliente;
 	
+	public int getNumCuentaDR() {
+		return NumCuentaDR;
+	}
+	public void setNumCuentaDR(int numCuentaDR) {
+		NumCuentaDR = numCuentaDR;
+	}
+	public double getValorRetiro() {
+		return valorRetiro;
+	}
+	public void setValorRetiro(double valorRetiro) {
+		this.valorRetiro = valorRetiro;
+	}
+	public double getValorDeposito() {
+		return valorDeposito;
+	}
+	public void setValorDeposito(double valorDeposito) {
+		this.valorDeposito = valorDeposito;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Cuenta getCuenta() {
+		return cuenta;
+	}
+	public void setCuenta(Cuenta cuenta) {
+		this.cuenta = cuenta;
+	}
+	public Cliente getCliente() {
+		return cliente;
+	}
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+	
 }
