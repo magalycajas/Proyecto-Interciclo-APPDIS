0c8aebf3cfc9ed3c2705754d7a0c1358817bc436
magalycajas <49215824+magalycajas@users.noreply.github.com>
2020-12-08 23:26:27 -0500

    Create Deposito_Retiro.java
    classe java

---
+++ Proyecto Interciclo/banco/src/main/java/ec/edu/ups/appdis/g1/modelo/Deposito_Retiro.java
@@ -0,0 +1,18 @@
+package ec.edu.ups.appdis.g1.modelo;
+
+import java.util.Date;
+
+import ec.edu.ups.appdis.g1.modelo.Cliente;
+import ec.edu.ups.appdis.g1.modelo.Cuenta;
+
+
+public class Deposito_Retiro {
+
+	private int NumCuentaDR;
+	private double valorRetiro;
+	private double valorDeposito;
+	private Date fecha;
+	private Cuenta cuenta;
+	private Cliente cliente;
+	
+}
