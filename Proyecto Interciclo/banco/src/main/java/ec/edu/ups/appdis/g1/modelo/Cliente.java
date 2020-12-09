23adf579258019ea4f1b82a4e4155a51c32ba1d3
magalycajas <49215824+magalycajas@users.noreply.github.com>
2020-12-08 21:05:15 -0500

    Administrativo
    rol de usuario

--- Proyecto Interciclo/banco/src/main/java/ec/edu/ups/appdis/g1/modelo/Cliente.java
+++ Proyecto Interciclo/banco/src/main/java/ec/edu/ups/appdis/g1/modelo/Cliente.java
@@ -1,24 +1,31 @@
 package ec.edu.ups.appdis.g1.modelo;
 
-public class cliente {
+public class Cliente {
+	
 	private String cedula;
 	private String nombre;
 	private String email;
+	
 	public String getCedula() {
 		return cedula;
 	}
+	
 	public void setCedula(String cedula) {
 		this.cedula = cedula;
 	}
+	
 	public String getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
---
+++ Proyecto Interciclo/banco/src/main/java/ec/edu/ups/appdis/g1/modelo/Usuario.java
@@ -0,0 +1,36 @@
+package ec.edu.ups.appdis.g1.modelo;
+
+public class Usuario {
+
+	private String cedula;
+	private String Nombre;
+	private String correo;
+	private String rol;
+	
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+	public String getNombre() {
+		return Nombre;
+	}
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+	public String getCorreo() {
+		return correo;
+	}
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+	public String getRol() {
+		return rol;
+	}
+	public void setRol(String rol) {
+		this.rol = rol;
+	}
+	
+	
+}
